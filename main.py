830cc04d568a6ae359efa6899596992f0713ba8d
DJ-Nikov <101722651+Discwebhook@users.noreply.github.com>
2025-01-16 08:21:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ### MODULES ###
 import copy as copy #Deep copy tables
 import pandas as pd #Dataframe to be used with Streamlit
-from streamlit_theme import st_theme #Dark/Light mode check
+from streamlit_theme import st-theme #Dark/Light mode check
 import streamlit as st #Data visualizer
 import time as time #Used for the "stopwatch" or "timer", optional
 import json as json #Used to write to or read from the sorted data file
